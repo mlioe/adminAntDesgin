fd39aec48f2b5da51c111a4cbaba60c60821f4fe
ljh <1147859273@qq.com>
2020-07-13 17:25:34 +0800

    加上菜单栏logo

--- src/components/index/menu.vue
+++ src/components/index/menu.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="box">
 		<!-- <menu-item :route="demoMenuRotes" :isCollapse="childrenIsCollapse" :style="!isCollapse?'width: 220px;':''"></menu-item> -->
+		<div class="ImgBox">
+			<img src="../../assets/logo.png" alt="" class="Img" />
+			<span v-show="!childrenIsCollapse">logo</span>
+		</div>
 		<menu-item :route="menuRoutes" :isCollapse="childrenIsCollapse" :style="!isCollapse ? 'width: 220px;' : ''"></menu-item>
 	</div>
 </template>
@@ -54,17 +58,18 @@ export default {
 		isCollapse(val) {
 			this.childrenIsCollapse = val;
 		},
-		screenWidth(val) {//如果要做h5导航菜单，则要通过这里判断页面大小，方便以后拖拽使用
+		screenWidth(val) {
+			//如果要做h5导航菜单，则要通过这里判断页面大小，方便以后拖拽使用
 			// console.log(val)
 			// 为了避免频繁触发resize函数导致页面卡顿，使用定时器
 			if (!this.timer) {
 				this.screenWidth = val;
 				this.timer = true;
 				let that = this;
-				setTimeout(()=>{
+				setTimeout(() => {
 					// 打印screenWidth变化的值
 					// console.log(that.screenWidth);
-					this.timer = false
+					this.timer = false;
 				}, 400);
 			}
 		}
@@ -72,7 +77,22 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .box {
+	.ImgBox {
+		height: 60px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: white;
+		width: 100%;
+	}
+	.Img {
+		width: 30px;
+		height: 30px;
+	}
+	.ImgBox span {
+		padding-left: 30px;
+	}
 }
 </style>

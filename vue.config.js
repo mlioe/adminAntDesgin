460e69812236c45a345c4d18f3b0d9f0d5aadf13
mlioe <1147859273@qq.com>
2020-07-21 21:00:58 +0800

    vue.config.js加上gzip压缩及打包路径配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 module.exports = {
+	// publicPath: './',
+	//设置文件压缩路径 /dist/根据服务器域名决定 如是www.baidu.com则是/ 如果是 www.baidu.com/dist/则需要打包成/dist/
+	publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
+	configureWebpack: (config) => {
+		if (process.env.NODE_ENV === 'production') { //GZIP压缩
+			return {
+				plugins: [new CompressionWebpackPlugin({
+					test: /\.(js|css)(\?.*)?$/i, //需要压缩的文件正则
+					threshold: 240, //文件大小大于这个值时启用压缩
+					deleteOriginalAssets: false //压缩后保留原文件
+				})]
+			}
+		}
+	},
 	devServer: {
 		proxy: {
 			'/api': {
@@ -7,7 +22,7 @@ module.exports = {
 				pathRewrite: { // pathRewrite表示路径重写，key表示一个正则，value表示别名 
 					'^/api': '' // 即用 '/api'表示'http://localhost:3000/api'
 				}
-			} 
+			}
 		}
 	}
 }
